8020a1045c6618762167a657e398f80ab085277f
Sven <sven@nederhoff.org>
2022-09-15 20:23:44 +0200

    add asyncio

--- src/folk_bot.py
+++ src/folk_bot.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import discord
+import asyncio
 from discord.ext import commands
 
 logging.basicConfig(level=os.getenv("LOG_LEVEL", "DEBUG").upper())
@@ -12,8 +13,20 @@ intents.voice_states = True
 
 bot = commands.Bot(command_prefix='=', intents=intents)
 
-for file in os.listdir("./cogs"):
-    if file.endswith(".py"):
-        await bot.load_extension(f"cogs.{file[:-3]}")
+async def main():
+    async with bot:
+        for file in os.listdir("./cogs"):
+            if file.endswith(".py"):
+                await bot.load_extension(f"cogs.{file[:-3]}")
+        await bot.load_extension('my_extension')
+        await bot.start(os.environ['BOT_TOKEN'])
+        #bot.run(os.environ['BOT_TOKEN'])
 
-bot.run(os.environ['BOT_TOKEN'])
+
+
+
+
+    
+        
+
+
