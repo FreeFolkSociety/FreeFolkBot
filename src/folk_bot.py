014c10667ef4d19a97efa47cf9fc3fa4b8c638dc
Sven <sven@nederhoff.org>
2022-09-15 20:46:23 +0200

    add client way ?

--- src/folk_bot.py
+++ src/folk_bot.py
@@ -11,13 +11,26 @@ log = logging.getLogger("FolkBot")
 intents = discord.Intents.default()
 intents.voice_states = True
 
-bot = commands.Bot(command_prefix='=', intents=intents)
+client = commands.Bot(command_prefix='=', intents=intents)
 
-for file in os.listdir("./cogs"):
-    if file.endswith(".py"):
-        bot.load_extension(f"cogs.{file[:-3]}")
-        # await bot.start(os.environ['BOT_TOKEN'])
-bot.run(os.environ['BOT_TOKEN'])
+async def load_extensions():
+    for file in os.listdir("./cogs"):
+        if file.endswith(".py"):
+            # cut off the .py from the file name
+            await client.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with client:
+        await load_extensions()
+        await client.start(os.environ['BOT_TOKEN'])
+
+asyncio.run(main())
+
+# for file in os.listdir("./cogs"):
+#     if file.endswith(".py"):
+#         bot.load_extension(f"cogs.{file[:-3]}")
+
+# bot.run(os.environ['BOT_TOKEN'])
 
 
 
